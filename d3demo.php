afb233547fb445f0a55abf3030379ac202f8f68f
Ashish K <ashishk2014@email.iimcal.ac.in>
2016-08-16 13:36:07 -0400

    Playing with the svg bar example shared by Scott Murray

--- d3demo.php
+++ d3demo.php
@@ -46,5 +46,56 @@
 			    return barHeight + "px";	
 			});
 		</script>
+		</br>
+	        The same bar graph now using svg (earlier we used divs)
+		<script type="text/javascript">
+			var w = 500;
+			var h = 100; 
+			var barPadding = 1;
+			
+			var dataset = [4,5,13,46,23,17,18,19,34,13,4,34,23,35,23,9,34];
+			
+			
+			var svg = d3.select("body")
+						.append("svg")
+						.attr("width", w)
+						.attr("height", h);
+
+			svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length);
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4);
+			   })
+			   .attr("width", w / dataset.length - barPadding)
+			   .attr("height", function(d) {
+			   		return d * 4;
+			   })
+			   .attr("fill", function(d) {
+					return "rgb(0, 0, " + (d * 10) + ")";
+			   });
+
+			svg.selectAll("text")
+			   .data(dataset)
+			   .enter()
+			   .append("text")
+			   .text(function(d) {
+			   		return d;
+			   })
+			   .attr("text-anchor", "middle")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4) + 14;
+			   })
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "11px")
+			   .attr("fill", "white");			
+		</script>
 	</body>
 </html>
